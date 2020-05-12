379dda9029ada3dfb2219d89b164aeb136016ba2
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-12 16:27:13 +0530

    learned keyboard and input events

---
+++ app8.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
+
+const select = document.querySelector('select');
+
+// Clear input
+taskInput.value='';
+
+
+// form.addEventListener('submit', runEvent);
+
+// // keydown
+// taskInput.addEventListener('keydown', runEvent);
+
+// // keyup
+// taskInput.addEventListener('keyup', runEvent);
+
+// keypress
+// taskInput.addEventListener('keypress', runEvent);
+
+// focus
+// taskInput.addEventListener('focus', runEvent);
+// blur
+// taskInput.addEventListener('blur', runEvent);
+
+// cut
+// taskInput.addEventListener('cut', runEvent);
+// paste
+// taskInput.addEventListener('paste', runEvent);
+
+// taskInput.addEventListener('input', runEvent);
+
+// change
+select.addEventListener('change', runEvent);
+
+function runEvent(e){
+    console.log(`EVENT TYPE: ${e.type}`);
+
+    // Get input value
+    // console.log(taskInput.value);
+
+    // e.preventDefault();
+
+    // console.log(e.target.value);
+    // heading.innerText = e.target.value;
+
+}
