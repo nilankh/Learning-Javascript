b625d154c36621dbdd11ebe82d1ab34a57d24a11
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-20 15:59:54 +0530

    read about constructtor and this keyword

--- oops/app11.js
+++ oops/app11.js
@@ -6,11 +6,23 @@
 // // console.log(brad.age);
 
 // Person constructor
-function Person(name) {
+function Person(name, dob) {
     this.name = name;
     // this key work refers to the current instance of the object
+    // this.age = age;    
+    this.birthday = new Date(dob);
+    this.calculateAge = function(){
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
 }
+// console.log(this);
+// this.alert(1);
 
-const brad = new Person('Brad');
-const john = new Person('John');
-console.log(john);
+// const brad = new Person('Brad', 36);
+// const john = new Person('John', 30);
+// console.log(john.age);
+
+const brad = new Person('Brad', '9-10-1981');
+console.log(brad.calculateAge());
