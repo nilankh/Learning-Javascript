7b89cc15754c136cc85ebdd5d6385a0476de8d96
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-01 17:00:40 +0530

    learning about this

--- thisCodeEvolu.js
+++ thisCodeEvolu.js
@@ -1,7 +1,8 @@
 var employee = {
     id: 1,
     greet: function() {
-        setTimeout(function() {console.log(this.id)}, 1000); 
+        var self = this;
+        setTimeout(function() {console.log(self.id)}, 1000); 
     }
 };
 employee.greet();
