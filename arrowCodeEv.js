3c0c11648e897a4af073d87eaeee3ba7f55b0d0e
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-01 16:45:12 +0530

    learning arrow function

--- arrowCodeEv.js
+++ arrowCodeEv.js
@@ -3,7 +3,8 @@ var getRegValue = function() {
 }
 console.log(getRegValue());
 
-const getArrowValue = () => {
-    return 12;
-}
+// const getArrowValue = () => {
+//     return 12;
+// }
+const getArrowValue = () => 12;
 console.log(getArrowValue());
