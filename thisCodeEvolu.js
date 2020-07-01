07a2df3193ae5db70f439b46a66b89271126fcc7
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-01 17:06:53 +0530

    done

--- thisCodeEvolu.js
+++ thisCodeEvolu.js
@@ -1,8 +1,17 @@
+// var employee = {
+//     id: 1,
+//     greet: function() {
+//         var self = this;
+//         setTimeout(function() {console.log(self.id)}, 1000); 
+//     }
+// };
+
 var employee = {
     id: 1,
     greet: function() {
-        var self = this;
-        setTimeout(function() {console.log(self.id)}, 1000); 
+        
+        setTimeout(() => {console.log(this.id)}, 1000); 
     }
+    // whenever we will use arrow function it is not going to create it own this, it is goiing to refer its parent this
 };
 employee.greet();
