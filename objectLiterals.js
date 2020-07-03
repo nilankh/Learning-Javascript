bb584c9763bd6da757ea3dd0b024b942dc6f958c
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-03 15:57:54 +0530

    started learning about object literals

---
+++ objectLiterals.js
@@ -0,0 +1,10 @@
+let firstName = "Nilank";
+let lastName = "Nikhil";
+
+let person = {
+    firstName: firstName,
+    lastName: lastName
+};
+
+console.log(person.firstName);
+console.log(person.lastName);
