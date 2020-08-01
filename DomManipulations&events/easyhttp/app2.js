b0f2640194a8c1443d709b8435a2ff2a7f943c16
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-01 18:04:00 +0530

    doone with custom http library(ajax with callbacks) part2(mid) done till post rrquest

--- DomManipulations&events/easyhttp/app2.js
+++ DomManipulations&events/easyhttp/app2.js
@@ -1,11 +1,35 @@
-const http = new easyHTTP2;
+const http = new easyHTTP2();
 
 // GET POSTS
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
-    if(err) {
-        console.log(err);
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+//     if(err) {
+//         console.log(err);
+//     }else{
+//         console.log(posts);
+//     }
+// });
+
+// get single post
+// GET POSTS
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post){
+//     if(err) {
+//         console.log(err);
+//     }else{
+//         console.log(post);
+//     }
+// });
+
+// Create data
+const data = {
+  title: "Custom Post",
+  body: "This is a custom post",
+};
+
+// create post
+http.post("https://jsonplaceholder.typicode.com/posts", data, function (err, post) {
+    if(err){
+        console.log(err)
     }else{
-        console.log(posts);
+        console.log(post)
     }
 });
-
--- DomManipulations&events/easyhttp/easyhttp2.js
+++ DomManipulations&events/easyhttp/easyhttp2.js
@@ -1,26 +1,40 @@
 function easyHTTP2() {
-    this.http = new XMLHttpRequest();
+  this.http = new XMLHttpRequest();
 }
 
 // Make a HTTP GET Request
-easyHTTP2.prototype.get = function(url, callback) {
-    this.http.open('GET', url, true);
-    
-    let self = this;
-    this.http.onload = function() {//without using arrow function
+easyHTTP2.prototype.get = function (url, callback) {
+  this.http.open("GET", url, true);
+
+  let self = this;
+  this.http.onload = function () {
+    //without using arrow function
     // this.http.onload = () => {
-        if(self.http.status === 200) {
-            callback(null, self.http.responseText);
-        }else{
-            callback('Error: ' + self.http.status)
-        }
+    if (self.http.status === 200) {
+      callback(null, self.http.responseText);
+    } else {
+      callback("Error: " + self.http.status);
     }
-    
-    this.http.send();
-}
+  };
+
+  this.http.send();
+};
 
 // Make a HTTP POST Request
+easyHTTP2.prototype.post = function (url, data, callback) {
+  this.http.open("POST", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+  let self = this;
+  this.http.onload = function () {
+    //without using arrow function
+    // this.http.onload = () => {
 
-// Make a HTTP PUT Request
+    callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
+
+
 
 // Make a HTTP DELETE Request
