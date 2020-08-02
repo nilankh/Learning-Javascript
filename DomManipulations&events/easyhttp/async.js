7dfe62565962678f3f016e61590336f80024f3fa
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-02 16:48:42 +0530

    custom http library(fetch with async await)

--- DomManipulations&events/easyhttp/async.js
+++ DomManipulations&events/easyhttp/async.js
@@ -1,4 +1,3 @@
-
 // async function myFunc() {
 //   const promise = new Promise((resolve, reject) => {
 //     setTimeout(() => resolve('Hello'), 1000);
@@ -19,14 +18,14 @@
 //   .catch(err => console.log(err));
 
 async function getUsers() {
-    // await response of the fetch call
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  
-    // Only proceed once its resolved
-    const data = await response.json();
-  
-    // only proceed once second promise is resolved
-    return data;
-  }
-  
-  getUsers().then(users => console.log(users)); 
+  // await response of the fetch call
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  // Only proceed once its resolved
+  const data = await response.json();
+
+  // only proceed once second promise is resolved
+  return data;
+}
+
+getUsers().then((users) => console.log(users));
